32eec3ad42e7f2011f7a37eaccc43280d0069af5
Fawadullah Imraj <fawadullah9911@gmail.com>
2026-09-01 22:40:01 +0500

    chore: clean unused project code and dependencies

--- lib/data/index.ts
+++
@@ -1,7 +0,0 @@
-﻿export * from './projects';
-export * from './services';
-export * from './technologies';
-export * from './leadership';
-export * from './testimonials';
-export * from './faqs';
-export * from './company';
